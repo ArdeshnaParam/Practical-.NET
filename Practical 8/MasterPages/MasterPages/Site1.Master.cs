760f988aa37b609fcb3ed8f36acc21fade07e537
ArdeshnaParam <46024778+ArdeshnaParam@users.noreply.github.com>
2019-04-04 08:38:20 +0530

    Add files via upload

---
+++ Practical 8/MasterPages/MasterPages/Site1.Master.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MasterPages
+{
+    public partial class Site1 : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        public Label LblHeader
+        {
+            get { return lblHeader; }
+        }
+        public Button BtnSearch
+        {
+            get { return btnSearch; }
+        }
+        public TextBox TxtSearch
+        {
+            get { return txtSearch; }
+        }
+    }
+}
